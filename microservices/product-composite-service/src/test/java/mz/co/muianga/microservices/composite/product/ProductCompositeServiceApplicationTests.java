ebfcce583a813d8071f7b217532819dd079e19f7
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2026-01-17 10:31:33 +0200

    chore: Initial commit

---
+++ microservices/product-composite-service/src/test/java/mz/co/muianga/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.muianga.microservices.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
