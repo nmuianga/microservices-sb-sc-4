5f06b37308369ac6888a1bb5aef802ed9d68dddf
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2026-02-28 05:28:02 +0200

    feat: chapter05 - adding an API description using OpenAPI

--- microservices/product-composite-service/src/main/java/mz/co/muianga/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/mz/co/muianga/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -16,17 +16,28 @@ import org.springframework.web.client.RestTemplate;
 @ComponentScan("mz.co.muianga")
 public class ProductCompositeServiceApplication {
 
-  @Value("${api.common.version}")         String apiVersion;
-  @Value("${api.common.title}")           String apiTitle;
-  @Value("${api.common.description}")     String apiDescription;
-  @Value("${api.common.termsOfService}")  String apiTermsOfService;
-  @Value("${api.common.license}")         String apiLicense;
-  @Value("${api.common.licenseUrl}")      String apiLicenseUrl;
-  @Value("${api.common.externalDocDesc}") String apiExternalDocDesc;
-  @Value("${api.common.externalDocUrl}")  String apiExternalDocUrl;
-  @Value("${api.common.contact.name}")    String apiContactName;
-  @Value("${api.common.contact.url}")     String apiContactUrl;
-  @Value("${api.common.contact.email}")   String apiContactEmail;
+  @Value("${api.common.version}")
+  String apiVersion;
+  @Value("${api.common.title}")
+  String apiTitle;
+  @Value("${api.common.description}")
+  String apiDescription;
+  @Value("${api.common.termsOfService}")
+  String apiTermsOfService;
+  @Value("${api.common.license}")
+  String apiLicense;
+  @Value("${api.common.licenseUrl}")
+  String apiLicenseUrl;
+  @Value("${api.common.externalDocDesc}")
+  String apiExternalDocDesc;
+  @Value("${api.common.externalDocUrl}")
+  String apiExternalDocUrl;
+  @Value("${api.common.contact.name}")
+  String apiContactName;
+  @Value("${api.common.contact.url}")
+  String apiContactUrl;
+  @Value("${api.common.contact.email}")
+  String apiContactEmail;
 
   @Bean
   RestTemplate restTemplate() {
@@ -52,8 +63,8 @@ public class ProductCompositeServiceApplication {
         .url(apiExternalDocUrl));
   }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCompositeServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProductCompositeServiceApplication.class, args);
+  }
 
 }
